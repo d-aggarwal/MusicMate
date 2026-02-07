9d3c0e1846d823ef068e0f0a6061074cc5d242b2
d-aggarwal <drishtiaggarwal6284@gmail.com>
2026-02-07 11:02:01 +0530

    Initialize NextJS Project

---
+++ app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        Hii there, welcome to my portfolio! I'm a software developer with a passion for creating innovative and efficient solutions. I specialize in web development and have experience with various programming languages and frameworks. Feel free to explore my projects and get in touch if you'd like to collaborate or learn more about my work.
+      </main>
+    </div>
+  );
+}
